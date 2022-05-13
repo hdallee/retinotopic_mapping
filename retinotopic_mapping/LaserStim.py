87149ac1ca7ee04179f2c610d93964234db9c980
abelpetik <axocska@gmail.com>
2022-05-13 12:23:23 +0200

    Update LED stimulation files and functions.

--- retinotopic_mapping/LaserStim.py
+++ retinotopic_mapping/LaserStim.py
@@ -2,21 +2,36 @@ from visexpa.engine.datahandlers.labjack import U3Wrap
 import time
 import pickle
 
+def stim_pulse(pulse_number, pulse_freq, pulse_width, lj):
+    for i in range(pulse_number):
+        lj.jack.setDIOState(IOnum=0, state=1)
+        time.sleep(pulse_width)
+        lj.jack.setDIOState(IOnum=0, state=0)
+        time.sleep(1/pulse_freq)
 
-
-def stim_start(animal_id, laser_power, rec_length):
+def stim_start(animal_id, laser_power, rec_length, stim_device='NP'):
     labjack = U3Wrap()
     labjack.jack.setDIOState(IOnum=0, state=0)
     stim_start_times = []
     t0 = time.time()
     time.sleep(2)
-    while time.time() - t0 < rec_length:
-        labjack.jack.setDIOState(IOnum=0, state=1)
-        stim_start_times.append(time.time())
-        print('Started laser stimulation.')
-        time.sleep(0.01)
-        labjack.jack.setDIOState(IOnum=0, state=0)
-        time.sleep(10)
+    if stim_device == 'NP':
+        while time.time() - t0 < rec_length:
+            labjack.jack.setDIOState(IOnum=0, state=1)
+            stim_start_times.append(time.time())
+            print('Started NP laser stimulation.')
+            time.sleep(0.01)
+            labjack.jack.setDIOState(IOnum=0, state=0)
+            time.sleep(10)
+    elif stim_device == 'Prizmatix':
+        pulse_number = 25
+        pulse_freq = 5
+        pulse_width = 0.002
+        while time.time() - t0 < rec_length:
+            print('Started Prizmatix LED stimulation.')
+            stim_start_times.append(time.time())
+            stim_pulse(pulse_number, pulse_freq, pulse_width, labjack)
+            time.sleep(10)
     format = '%Y%m%d-%H%M%S'
     save_time = time.strftime(format, time.localtime(t0))
     filename = save_time + '_' + animal_id + '_' + laser_power + '_stimulation_starts' + '.pickle'
--- retinotopic_mapping/examples/experiment_templates/fluorescent_imaging/laser_power_fluorescent_experiment.py
+++ retinotopic_mapping/examples/experiment_templates/fluorescent_imaging/laser_power_fluorescent_experiment.py
@@ -1,16 +1,25 @@
+import time
 from multiprocessing import Process
 from visexpa.engine.datahandlers.teledyne_camera import record
 import retinotopic_mapping.StimulusRoutines as stim
 from retinotopic_mapping import LaserStim
+from visexpa.engine.datahandlers.labjack import LED_stimulation
 from retinotopic_mapping.tools.IO.log_file_extraction import save_log_to_recording
 
-animal_id = 'Rat_no57'
-comment = ''  # Eg: laser_power_50 or not_dark_adapted etc.
+animal_id = 'Rat_no58'
+comment = 'laser_power_50'  # Eg: laser_power_50 or not_dark_adapted etc.
+stimulator_device = 'Prizmatix' # NP if Neurophotometrics laser is used, Prizmatix if Prizmatix LED is used
 
 grating_width = 0.1  # in cycles/visual degree
 temporal_frequency = 1.5  # in cycles/second
 
-recording_length = 300  # in sec
+recording_length = 30  # in sec
+
+target_LED_power = 10
+LED_config = cfg = {'wait_pre_s': 0, 'pulse_width_s': 0.2, 'pulse_period_s': 1.0}
+
+folder = time.strftime('%Y.\\%m.%d.\\data')
+LED_log_filename = time.strftime('%Y%m%d-%H%M%S-' + comment + '-led_log.pkl')
 
 if __name__ == '__main__':
     # Set up recording
@@ -20,8 +29,14 @@ if __name__ == '__main__':
     # Set up stimulation
 
 
-    stimulator_process = Process(target=LaserStim.stim_start, kwargs={'animal_id': animal_id, 'laser_power': comment,
-                                                                      'rec_length': recording_length})
+    stimulator_process = Process(target=LED_stimulation, kwargs={'TTL_config': LED_config,
+                                                                 'target_LED_current': target_LED_power,
+                                                                 'len': recording_length,
+                                                                 'outfile_path': 'D:\\Data\\Experiment\\' + folder +
+                                                                                 '\\' + LED_log_filename})
+    # stimulator_process = Process(target=LaserStim.stim_start, kwargs={'animal_id': animal_id, 'laser_power': comment,
+    #                                                                   'rec_length': recording_length,
+    #                                                                   'stim_device': stimulator_device})
 
     # Start recording
     recorder_process.start()
