58165e57c2de9ce8d14d3895f1cf88c8ba659838
abelpetik <axocska@gmail.com>
2022-10-19 14:56:19 +0200

    Add monitors.py for storing monitor parameters and easy setting in experiment scripts. Retinotopy_fluorescent_experiment.py now stops recording automatically when the stimulation is finished.

--- retinotopic_mapping/examples/experiment_templates/fluorescent_imaging/full_screen_flash_fluorescent_experiment.py
+++ retinotopic_mapping/examples/experiment_templates/fluorescent_imaging/full_screen_flash_fluorescent_experiment.py
@@ -6,11 +6,14 @@ from multiprocessing import Process, Event
 from visexpa.engine.datahandlers.teledyne_camera import record
 import retinotopic_mapping.StimulusRoutines as stim
 from retinotopic_mapping.MonitorSetup import Monitor, Indicator
+from retinotopic_mapping.tools.monitors import monitors
 from retinotopic_mapping.DisplayStimulus import DisplaySequence
 from retinotopic_mapping.tools.IO.log_file_extraction import save_log_to_recording
 
 animal_id = 'Rat_no57'
 comment = ''  # Eg: laser_power_50 or not_dark_adapted etc.
+monitor = 'Dell_24_inch'  # Choose one from the dictionary in retinotopic_mapping/tools/monitors.py
+monitor_distance = 30  # Distance of monitor from animal's eye in cm
 
 # grating_width = 0.1  # in cycles/visual degree
 temporal_frequency = 2  # in Hz
@@ -34,9 +37,8 @@ if __name__ == '__main__':
     recorder_process = Process(target=record, kwargs={'config': 'from_file', 'length': None, 'end_flag':stim_finished_flag, 'TTL': False, 'comment': animal_id + '_' + comment})
     
     # Set up stimulation
-    # Using Fujitsu DY22T-7
-    # mon = Monitor(resolution=(1920, 1080), dis=60., mon_width_cm=88.6, mon_height_cm=49.8, downsample_rate=4, refresh_rate=60.0)
-    mon = Monitor(resolution=(1920, 1080), dis=30., mon_width_cm=47.6, mon_height_cm=26.7, downsample_rate=4, refresh_rate=60.0)
+    mon = Monitor(resolution=monitors[monitor]['resolution'], dis=monitor_distance, mon_width_cm=monitors[monitor]['width'],
+                  mon_height_cm=monitors[monitor]['height'], downsample_rate=4, refresh_rate=60.0)
 
     # The next 2 lines are a workaround for hiding the indicator.
     dummy = Monitor(resolution=(0,0), dis=1, mon_width_cm=1, mon_height_cm=1)
---
+++ retinotopic_mapping/tools/monitors.py
@@ -0,0 +1,8 @@
+"""
+Definition of the parameters of the monitors used in the lab.
+Add to this dictionary if you want to use a new screen!
+"""
+
+monitors = {'Samsung_LE40C530': {'width': 88.6, 'height': 49.8, 'resolution': (1920, 1080)},
+              'Fujitsu_rodent': {'width': 47.6, 'height': 26.7, 'resolution': (1920, 1080)},
+              'Dell_24_inch': {'width': 52.5, 'height': 29.5, 'resolution': (1920, 1080)}}
